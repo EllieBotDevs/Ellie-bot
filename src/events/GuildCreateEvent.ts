05505b0468050f0faf5e3973c8d3fecbcb7271d1
EmotionChild <JDRCaple@outlook.com>
2022-01-13 23:21:37 +1300

    Updated and added stuff

---
+++ src/events/GuildCreateEvent.ts
@@ -0,0 +1,32 @@
+// https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildCreate
+import { Guild } from 'discord.js';
+import BaseEvent from '../utils/structures/BaseEvent';
+import DiscordClient from '../client/client';
+import { getRepository } from 'typeorm';
+import { GuildConfiguration } from '../typeorm/entities/GuildConfiguration';
+
+export default class GuildCreateEvent extends BaseEvent {
+  constructor(
+    private readonly guildConfigRepository = getRepository (GuildConfiguration)
+  ) {
+    super('guildCreate');
+  }
+  
+  async run(client: DiscordClient, guild: Guild) {
+    console.log('Hellp World!');
+    console.log(`Joined ${guild.name}`);
+
+    const config = await this.guildConfigRepository.findOne({
+      guildId: guild.id,
+    });
+    if (config) {
+      console.log('A configuration was found!');
+    } else {
+      console.log('A configuration was not found. Creating One.');
+      const newConfig = this.guildConfigRepository.create({
+        guildId: guild.id,
+      })
+      return this.guildConfigRepository.save(newConfig);
+    }
+  }
+}
