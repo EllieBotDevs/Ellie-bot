e58cc7ea20ba665ee23fbe80dbb1ce476ad86414
EmotionChild <JDRCaple@outlook.com>
2022-01-27 18:13:06 +1300

    Added stuff and updated dependencies

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,21 +1,31 @@
 import { Client, ClientOptions, Collection } from 'discord.js';
 import BaseEvent from '../utils/structures/BaseEvent';
 import BaseCommand from '../utils/structures/BaseCommand';
+import { GuildConfiguration } from '../typeorm/entities/GuildConfiguration';
 
 export default class DiscordClient extends Client {
 
   private _commands = new Collection<string, BaseCommand>();
   private _events = new Collection<string, BaseEvent>();
   private _prefix: string = '!';
+  private _configs = new Collection<string, GuildConfiguration>();
 
   constructor(options: ClientOptions) {
     super(options);
   }
 
-  get commands(): Collection<string, BaseCommand> { return this._commands; }
-  get events(): Collection<string, BaseEvent> { return this._events; }
-  get prefix(): string { return this._prefix; }
+  get commands(): Collection<string, BaseCommand> {
+    return this._commands;
+  }
+  get events(): Collection<string, BaseEvent> {
+    return this._events;
+  }
+  get prefix(): string {
+    return this._prefix;
+  }
 
-  set prefix(prefix: string) { this._prefix = prefix; }
+  set prefix(prefix: string) {
+    this._prefix = prefix;
+  }
 
 }
