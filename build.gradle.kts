f11fe05859adaebeb2893714ca3412d28d8de924
EmotionChild <JDRCaple@outlook.com>
2022-08-06 14:44:33 +1200

    More stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,31 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
+val numberVersion = "5.0.0"
+
 group = "net.ellibot.ellie"
-version = "1.0-SNAPSHOT"
+version = "5.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    maven("https://m2.dv8tion.net/releases")
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:5.0.0-alpha.17")
+    implementation(group = "net.dv8tion", name = "JDA", version = "5.0.0-alpha.17") {
+        exclude(module = "opus-java")
+    }
+    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.10")
+    implementation(group = "com.github.JDA-Applications", name = "JDA-Utilities", version = "804d58a") {
+        // This is fine
+        exclude(module = "jda-utilities-examples")
+        exclude(module = "jda-utilities-doc")
+        exclude(module = "jda-utilities-command")
+        exclude(module = "jda-utilities-menu")
+        exclude(module = "jda-utilities-oauth2")
+    }
 }
